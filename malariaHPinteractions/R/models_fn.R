711782a3a6716dd7e5f14df00f19a62f17c4dec3
Parnika <parnika91@gmail.com>
2020-06-24 17:32:22 +0200

    comments removed

--- malariaHPinteractions/R/models_fn.R
+++ malariaHPinteractions/R/models_fn.R
@@ -1,5 +1,3 @@
-# pfE_bp <- loadRData("ERP106451/cor/ERP106451_int_bipartite.RData")
-# 
 library(dplyr)
 library(igraph)
 library(betareg)
@@ -78,48 +76,3 @@ save(join_df, file = paste0(org, "_", net, "_nwstats.RData", collapse = ""))
 load("Unweighted_RGR_MIS_cleaned.RData")
 RGR_MIS_cleaned <- inner_join(join_df, RGR_MIS_cleaned)
 save(RGR_MIS_cleaned, file = "Unwebp_ighted_RGR_MIS_cleaned.RData")
-
-# pb_pp_bw <- betweenness(ig_pb_pp, v = V(ig_pb_pp), directed = FALSE)
-# pb_pp_cl <- closeness(ig_pb_pp, vids = V(ig_pb_pp))
-# pb_pp_dg <- degree(ig_pb_pp, v = V(ig_pb_pp))
-# 
-# pfE_bp_ig <- graph_from_data_frame(pfE_bp, directed = F)
-# 
-# # a. ebp_igen centrality
-# 
-# pfE_bp_ig_ec <- eigen_centrality(pfE_bp_ig, directed = FALSE)
-# 
-# # b. k-shells or k-core
-# pfE_bp_ig_kc <- coreness(pfE_bp_ig, mode = c("all"))
-# # c. degree
-# pfE_bp_ig_dg <- degree(pfE_bp_ig, v = V(pfE_bp_ig))
-# # d. betweenness
-# pfE_bp_ig_bw <- betweenness(pfE_bp_ig, directed = FALSE, v = V(pfE_bp_ig))
-# # e. closeness
-# pfE_bp_ig_cl <- closeness(pfE_bp_ig, vids = V(pfE_bp_ig))
-# 
-# pfE_bp_ig_dg_p <- pfE_bp_ig_dg[grep(pattern = "p_OG", names(pfE_bp_ig_dg))]
-# pfE_bp_ig_dg_df <- as.data.frame(pfE_bp_ig_dg_p) %>% 
-#   tibble::rownames_to_column("Orthogroup")
-# 
-# pfE_bp_ig_bw_p <- pfE_bp_ig_bw[grep(pattern = "p_OG", names(pfE_bp_ig_bw))]
-# pfE_bp_ig_bw_df <- as.data.frame(pfE_bp_ig_bw_p) %>% 
-#   tibble::rownames_to_column("Orthogroup")
-# 
-# pfE_bp_ig_cl_p <- pfE_bp_ig_cl[grep(pattern = "p_OG", names(pfE_bp_ig_cl))]
-# pfE_bp_ig_cl_df <- as.data.frame(pfE_bp_ig_cl_p) %>% 
-#   tibble::rownames_to_column("Orthogroup")
-# 
-# pfE_bp_ig_ec_p <- pfE_bp_ig_ec$vector[grep(pattern = "p_OG", names(pfE_bp_ig_ec$vector))]
-# pfE_bp_ig_ec_df <- as.data.frame(pfE_bp_ig_ec_p) %>% 
-#   tibble::rownames_to_column("Orthogroup")
-# 
-# pfE_bp_ig_kc_p <- pfE_bp_ig_kc[grep(pattern = "p_OG", names(pfE_bp_ig_kc))]
-# pfE_bp_ig_kc_df <- as.data.frame(pfE_bp_ig_kc_p) %>% 
-#   tibble::rownames_to_column("Orthogroup")
-# 
-# join_pfE_bp <-plyr::join_all(list(pfE_bp_ig_dg_df,pfE_bp_ig_bw_df, pfE_bp_ig_cl_df,
-#                                   pfE_bp_ig_ec_df, pfE_bp_ig_kc_df), by = "Orthogroup", type = "full")
-# join_pfE_bp[is.na(join_pfE_bp)] <- 0
-# RGR_MIS_cleaned <- inner_join(join_pfE_bp, RGR_MIS_cleaned)
-# save(RGR_MIS_cleaned, file = "Unwebp_ighted_RGR_MIS_cleaned.RData")
