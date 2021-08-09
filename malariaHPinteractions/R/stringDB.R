9eaaeaab28ec80ee6c29143b814af4af8107d6e0
Parnika <parnika91@gmail.com>
2021-08-09 23:44:19 +0200

    intrasp DDI function

--- malariaHPinteractions/R/stringDB.R
+++ malariaHPinteractions/R/stringDB.R
@@ -3,40 +3,77 @@ library(tidyverse)
 library(seqinr)
 
 # load string db interactions
-Pb <- read.csv("~/Downloads/5821.protein.links.full.v11.0.txt", sep="")
-Mm <- read.csv("~/Downloads/10090.protein.links.full.v11.0.txt", sep="")
+Pb <- read.csv("Downloads/5821.protein.links.full.v11.0.txt", sep="")
+Mm <- read.csv("10090.protein.links.full.v11.0.txt", sep="")
 
 # load orthogroups
-pOG <- read.delim("~/Documents/Data/parasite_orthogroups.txt")
-hOG <- read.delim("~/Documents/Data/host_orthogroups.txt")
-
-# remove taxon ID from protein names from stringdb files
-Pb[,1] <- substr(Pb[,1], 6, nchar(Pb[,1]))
-Pb[,2] <- substr(Pb[,2], 6, nchar(Pb[,2]))
-
-Mm[,1] <- substr(Mm[,1], 7, nchar(Mm[,1]))            
-Mm[,2] <- substr(Mm[,2], 7, nchar(Mm[,2]))
-
-# Make all possible combinations with the genes in orthogroups
-Pb_comb <- expand.grid(pOG$Pberghei, pOG$Pberghei); colnames(Pb_comb) <- c("protein1", "protein2")
-Pb_stringdb <- inner_join(Pb[,1:2], Pb_comb)
-
-Mm_comb <- expand.grid(hOG$mouse, hOG$mouse); colnames(Mm_comb) <- c("protein1", "protein2")
-Mm_stringdb <- inner_join(Mm[,1:2], Mm_comb)
-
-# get a list of proteins so that we can obtain their PFAM domains
-Pb_list <- unique(c(Pb_stringdb[,1], Pb_stringdb[,2]))
-Pb_list <- as.data.frame(Pb_list)
-write.csv(Pb_list, "Pb_list_Pfram_entries.csv", row.names = F)
+pOG <- read.delim("Downloads//parasite_orthogroups.txt")
+hOG <- read.delim("Downloads//host_orthogroups.txt")
 
 ### Annotated protein sequences downloaded of version 49, 2020-10-27 from PlasmoDB
-# load protein sequence fasta file
-Pb_seq <- read.fasta("Downloads/Pberghei_proteins.fasta", seqtype = "AA", as.string = T)
-# remove ".1-p1" from the names
-names(Pb_seq)<- substr(names(Pb_seq), 1, nchar(names(Pb_seq))-5)
-Pb_entries_names <- sapply(pOG$Pberghei, function(x) grep(pattern = x, names(Pb_seq)))
-Pb_entries_seq <- Pb_seq[unlist(Pb_entries_names)]
+## load protein sequence fasta file
 
-write.fasta(Pb_entries_seq[1:2000], names = names(Pb_entries_seq)[1:2000], file.out = "Pberghei_Pfam_entries_first.fasta")
-write.fasta(Pb_entries_seq[2001:4006], names = names(Pb_entries_seq)[2001:4006], file.out = "Pberghei_Pfam_entries_second.fasta")
+# Pb_seq <- read.fasta("Downloads/Pberghei_proteins.fasta", seqtype = "AA", as.string = T)
+# ## remove ".1-p1" from the names
+# names(Pb_seq)<- substr(names(Pb_seq), 1, nchar(names(Pb_seq))-5)
+# Pb_entries_names <- sapply(pOG$Pberghei, function(x) grep(pattern = x, names(Pb_seq)))
+# Pb_entries_seq <- Pb_seq[unlist(Pb_entries_names)]
+# 
+# write.fasta(Pb_entries_seq[1:2000], names = names(Pb_entries_seq)[1:2000], file.out = "Pberghei_Pfam_entries_first.fasta")
+# write.fasta(Pb_entries_seq[2001:4006], names = names(Pb_entries_seq)[2001:4006], file.out = "Pberghei_Pfam_entries_second.fasta")
+
+# get domains and remove superfamily rows
+d1 <- read.csv("Downloads/PbergheiPFAM_1_2_hitdata.txt", stringsAsFactors=TRUE) %>%
+  filter(!(Hit.type == "superfamily"))
+d2 <- read.csv("Downloads/PbergheiPFAM_2_2_hitdata.txt", stringsAsFactors=TRUE) %>%
+  filter(!(Hit.type == "superfamily"))
+d <- rbind(d1, d2)
+
+### intra-species DDIs function
+intraspecies_DDIs <- function(PPI, OG, D){
+  
+  # remove taxonID from PPIs
+  PPI[,1] <- sapply(PPI[,1], function(x) strsplit(x, split = "\\.")[[1]][2])
+  PPI[,2] <- sapply(PPI[,2], function(x) strsplit(x, split = "\\.")[[1]][2])
+  
+  # Put Orthogroup names next to protein names
+  P1 <- as.data.frame(PPI[,1]); colnames(P1) <- "Pberghei"
+  P2 <- as.data.frame(PPI[,2]); colnames(P2) <- "Pberghei"
+  OG1 <- inner_join(P1, OG[,c("Orthogroup", "Pberghei")])
+  OG2 <- inner_join(P2, OG[,c("Orthogroup", "Pberghei")])
+  
+  ppi_og <- cbind(OG1, OG2); colnames(ppi_og) <- c("P1", "OG1", "P2", "OG2")
+  
+  # add their domain and expand the rows
+  ## remove "Q - >" part of Query column of domains to retain only the protein name
+  D[,1] <- sapply(D[,1], function(x) strsplit(as.character(x), split = ">")[[1]][2])
+  D[,1] <- substr(D[,"Query"], 1, 13) # conversion to previous ID
+  # add domains to proteins -> could be multiple rows for one protein
+  # P1 OG1 D1
+  # P1 OG1 D2
+  ppi_og_d <- rbind(OG1, OG2) %>%
+    distinct() %>%
+    left_join(., D[,c("Query", "Accession")], by = c("Pberghei" = "Query"))
+  
+  # put this info into the ppi_og dataframe
+  dom1 = left_join(ppi_og, ppi_og_d, by = c("P1" = "Pberghei")) %>%
+    rename(Dom1 = Accession)
+  
+  dom2 = left_join(dom1, ppi_og_d, by = c("P2" = "Pberghei")) %>%
+    select(-c(5,7)) %>%
+    na.omit() %>%
+    rename(Dom2 = Accession)
+  
+  ddi <- dom2[,c("Dom1", "Dom2")]
+ 
+  # universe of domain interactions
+  domains <- na.omit(unique(as.character(ppi_og_d$Accession)))
+  d_uni <- expand.grid(domains, domains)
+  
+  # takes a very long time
+  x <- d_uni[!duplicated(t(apply(d_uni, 1, sort))), ]
+  y <- ddi[!duplicated(t(apply(ddi, 1, sort))), ]
+  
+ }
+
  
